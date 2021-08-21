a201a3f1f96bc7d638d2dd7615a9a3c513384e93
harshilp1902 <harshilp1902@gmail.com>
2021-08-21 17:45:39 +0530

    Testing

--- model.py
+++ model.py
@@ -4,23 +4,19 @@ Created on Thu Aug 19 19:40:40 2021
 
 @author: ARJYA
 """
-from flask import Flask,render_template,url_for,request
 import json
+import redis
 
-# load the model from disk
-json_file_path='New Text Document.json'
-with open(json_file_path, 'r') as j:
-     data = json.loads(j.read())
-     
-app = Flask(__name__)
-
-@app.route('/data',methods=['POST','GET'])
-def data():
-    if request.method == 'POST':
-        
-        return data
-    else:
-        return data
-
-if __name__ == '__main__':
-	app.run(debug=True)
+def data(data):
+  
+# Data to be written
+    dictionary ={
+        "name" : data
+    }
+      
+    # Serializing json 
+    json_object = json.dumps(dictionary, indent = 1)
+      
+    # Writing to sample.json
+    with open("sample.json", "w") as outfile:
+        outfile.write(json_object)
