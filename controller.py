c01fb1dfe0aa5e57ec95a27482513592b1d0ec06
harshilp1902 <harshilp1902@gmail.com>
2021-08-26 14:10:45 +0530

    Included redis server to store data in cache memory

--- controller.py
+++ controller.py
@@ -7,17 +7,25 @@ Created on Wed Aug 18 21:13:52 2021
 
 from flask import Flask, jsonify
 from flask_restx import Resource
+import redis
+
+r = redis.Redis(host='localhost',port=6379)
+
+data={"name":"Arjya Basu","gender":"male","Age":"22"}
+
+for i in data:
+    r.set(i,data[i])
 
 class HelloWorld(Resource):
 
     def get(self):
-        return jsonify({"Hello":"get"})
+        return jsonify({"Hello": str(r.get("name"))[2:-1]})
     
     def post(self):
-        return jsonify({"Hello": "post"})
+        return jsonify({"Hello": str(r.get("Age"))[2:-1]})
 
     def delete(self):
-        return jsonify({"Hello": "delete"})
+        return jsonify({"Hello": str(r.get("gender"))[2:-1]})
 
     def put(self):
-        return jsonify({"Hello":"put"})
+        return jsonify({"Hello": "put"})
