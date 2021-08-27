ecd63d067d0de6bc489f20d94fa01e0b01d495bc
harshilp1902 <harshilp1902@gmail.com>
2021-08-28 01:15:53 +0530

    class was created

--- model.py
+++ model.py
@@ -1,63 +1,55 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Aug 26 19:53:40 2021
-
-@author: ARJYA
-"""
 import json
-'''
-import redis
 
-r = redis.Redis(host='localhost',port=6379)
-  '''
-# Opening JSON file
-f = open('sample.json',)
-  
-# returns JSON object as 
-# a dictionary
-data = json.load(f)
-
-def display():
+class DataHandling:
+    global data
+    # Opening JSON file
+    f = open('sample.json',)
+          
+    # returns JSON object as 
+    # a dictionary
+    data = json.load(f)
     
-    return data
-
-def display_by_id(id):
+    def display(self):
+        
+        return data
     
-    return data[id]
-
-def add(dataset):
-    data[str(len(data)+1)]=dataset
-    json_object = json.dumps(data)
-  
-    # Writing to sample.json
-    with open("sample.json", "w") as outfile:
-        outfile.write(json_object)
+    def display_by_id(self,id):
         
-    return data
-
-def delete(ids):
-    del data[ids]
-    json_object = json.dumps(data)
-  
-    # Writing to sample.json
-    with open("sample.json", "w") as outfile:
-        outfile.write(json_object)
-        
-    return "The information of row "+ids+" is deleted"
-
-def update(ids,name,age,gender):
-    d1={}
-    if name!=None:
-        d1.update({"name":name})
-    if age!=None:
-        d1.update({"Age":age})
-    if gender!=None:
-        d1.update({"gender":gender})
-    data.update({ids:d1})
-    json_object = json.dumps(data)
-  
-    # Writing to sample.json
-    with open("sample.json", "w") as outfile:
-        outfile.write(json_object)
-        
-    return "The dictionary was updated"
+        return data[id]
+    
+    def add(self,dataset):
+        data[str(len(data)+1)]=dataset
+        json_object = json.dumps(data)
+      
+        # Writing to sample.json
+        with open("sample.json", "w") as outfile:
+            outfile.write(json_object)
+            
+        return data
+    
+    def delete(self,ids):
+        del data[ids]
+        json_object = json.dumps(data)
+      
+        # Writing to sample.json
+        with open("sample.json", "w") as outfile:
+            outfile.write(json_object)
+            
+        return "The information of row "+ids+" is deleted"
+    
+    def update(self,ids,name,age,gender):
+        d1={}
+        if name!=None:
+            d1.update({"name":name})
+        if age!=None:
+            d1.update({"Age":age})
+        if gender!=None:
+            d1.update({"gender":gender})
+        data.update({ids:d1})
+        json_object = json.dumps(data)
+      
+        # Writing to sample.json
+        with open("sample.json", "w") as outfile:
+            outfile.write(json_object)
+            
+        return "The dictionary was updated"
