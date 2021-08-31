45b29ff2780e8961e706677ddb61540d6acee9ad
Arjya1999 <1999arjobasu@gmail.com>
2021-08-31 15:30:21 +0530

    oops concepts,code readability,error handling & git global config was updated

--- controller.py
+++ controller.py
@@ -3,54 +3,51 @@ from flask_restx import Resource
 from model import *
 
 class RecordHandling(Resource):
-    global var
-    var=DataHandling()
+    
+    def __init__(self):
+        self.var=DataHandling()
+    
     def get(self):
-        ids = request.get_json()
-        print(ids)
-        if ids== None:
-            try:
-                display_all_records= var.display()
-                return jsonify({"status":200,"message":"OK, Returning data from get method","data": display_all_records})
-            except:
-                return jsonify({"status":400, "message":"Bad Request, Some error has occured while returning data from get method","data":[]})
-        else:
-            try:
-                display_record_by_id= var.display_by_id(ids['ids'])
-                return jsonify({"status":200,"message":"OK, Returning data from get method","data": display_record_by_id})
-            except:
-                return jsonify({"status":400, "message":"Bad Request, Some error has occured while returning data from get method","data":[]})
+        try:
+            ids = request.get_json()
+            
+            if ids== None:
+                display_all_records= self.var.display()
+                return jsonify({"success":True,"message":"OK, Returning data from get method","data": display_all_records})
+            else:
+                display_record_by_id= self.var.display_by_id(ids['ids'])
+                return jsonify({"success":True,"message":"OK, Returning data from get method","data": display_record_by_id})
+        except:
+            return jsonify({"success":False, "message":"Bad Request, Some error has occured while returning data from get method","data":[]})
     
     def post(self):
-        data=request.get_json()
-        print(data)
-        #name = request.args.getlist('name')
-        #Age = request.args.getlist('Age')
-        #gender = request.args.getlist('gender')
         try:
-            result=var.add({"name":data['name'],"Age":data['Age'],"gender":data['gender']})
-            return jsonify({"status":200,"message":"OK, Returning data from post method","data": result})
+            data_request_json = request.get_json()
+            dataset = {}
+            dataset.update({"name":data_request_json['name'],"Age":data_request_json['Age'],"gender":data_request_json['gender']})
+            
+            results = self.var.add(dataset)
+            
+            return jsonify({"success":True,"message":"OK, Returning data from post method","data": results})
         except:
-            return jsonify({"status":400, "message":"Bad Request, Some error has occured while returning data from post method","data":[]})
+            return jsonify({"success":False, "message":"Bad Request, Some error has occured while returning data from post method","data":[]})
         
     def delete(self):
-        ids =request.get_json()
-        #ids= request.args.getlist('ids')
         try:
-            info=var.delete(ids['ids'])
-            return jsonify({"status":200,"message":"OK, Returning data from delete method","data": info['msg']})
+            data_request_json =request.get_json()
+            
+            info = self.var.delete(data_request_json['ids'])
+            
+            return jsonify({"success":True,"message":"OK, Returning data from delete method","data": info['message']})
         except:
-            return jsonify({"status":400, "message":"Bad Request, Some error has occured while returning data from delete method","data":[]})
+            return jsonify({"success":False, "message":"Bad Request, Some error has occured while returning data from delete method","data":[]})
 
     def put(self):
-        data=request.get_json()
-        print(data)
-        #ids= request.args.getlist('ids')
-        #name = request.args.getlist('name')
-        #Age = request.args.getlist('Age')
-        #gender = request.args.getlist('gender')
         try:
-            result=var.update(data['ids'],data['name'],data['Age'],data['gender'])
-            return jsonify({"status":200,"message":"OK, Returning data from put method","data": result['msg']})
+            data_request_json = request.get_json()
+        
+            results = self.var.update(data_request_json['ids'], data_request_json['name'], data_request_json['Age'], data_request_json['gender'])
+            
+            return jsonify({"status":200,"message":"OK, Returning data from put method","data": results['message']})
         except:
             return jsonify({"status":400, "message":"Bad Request, Some error has occured while returning data from put method","data":[]})
