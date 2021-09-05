d448ee3b0d8e812f64d4c82ab36d22e413872760
Arjya1999 <1999arjobasu@gmail.com>
2021-09-05 19:25:07 +0530

    Json returning function for histogram was coded

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import json
 from config import ConfigProduction
+import pandas as pd
 
 class DataHandling:
     
@@ -49,3 +50,20 @@ class DataHandling:
             outfile.write(json_object)
             
         return {"message":"The dictionary was updated"}
+    
+class HistogramHandling:
+    
+    def __init__(self):
+        self.dataset = pd.read_csv("mlb_players.csv")
+    
+    def histogram(self,column_name):
+        
+        data= self.dataset[column_name].value_counts()
+        name = self.dataset.Position.unique()
+        data = self.dataset['Position'].value_counts()
+        y_axis= self.dataset['Position'].value_counts().tolist()
+        x_axis = data.index.tolist()
+        
+        dic={"x_axis":x_axis,"y_axis":y_axis}
+    
+        return dic
